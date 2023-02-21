c9e24d5cea8f891e0c546b2cb7f24727ef37a0a2
lilble <77142800+lilble@users.noreply.github.com>
2023-02-22 04:53:56 +0900

    #1.2

--- src/server.js
+++ src/server.js
@@ -1,12 +1,20 @@
+import http from "http"
+import WebSocket from "ws";
 import express from "express";
 
 const app = express();
 
 app.set("view engine", "pug"); // template engine의 종류 : pug
 app.set("views", __dirname + "/views"); // template file 위치
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (req, res) => res.render("home")); // route handler - rendering "home.pug"
+app.get("/*", (req, res) => res.redirect("/"));
 
-app.get("/", (req, res) => res.render("home")); // template file (home.pug)
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+// app.listen(3000, handleListen);
 
-console.log("hello");
+// 같은 서버에서 http, web socket 둘 다 작동 가능
+const server = http.createServer(app);  
+const wss = new WebSocket.Server({server}); 
 
-app.listen(3000);
+server.listen(3000, handleListen);
