75a3398b8d0a3388fde16a1ffb38186edbef651a
lilble <77142800+lilble@users.noreply.github.com>
2023-05-22 19:16:55 +0900

    #3.2 camera switch

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,19 +1,92 @@
 const socket = io();
 
 const myFace = document.getElementById("myFace");
+const muteBtn = document.getElementById("mute");
+const cameraBtn = document.getElementById("camera");
+const camerasSelect = document.getElementById("cameras");
 
 let myStream;
+let muted = false;
+let cameraOff = false;
 
-async function getMedia() {
+async function getCameras() {
   try {
-    myStream = await navigator.mediaDevices.getUserMedia({
-      audio: true,
-      video: true,
+    const devices = await navigator.mediaDevices.enumerateDevices();
+    // devices.forEach((device) => {
+    //   if (device.kind === "videoinput") {
+    //     console.log(device.label);
+    //   }
+    // });
+    const cameras = devices.filter((device) => device.kind === "videoinput");
+    const currentCamera = myStream.getVideoTracks()[0];
+    cameras.forEach((camera) => {
+      const option = document.createElement("option");
+      option.value = camera.deviceId;
+      option.innerText = camera.label;
+      if (currentCamera.label == camera.label) {
+        option.selected = true; // 처음에 [나오는 영상 != 선택된 영상]의 오류 해결
+      }
+      camerasSelect.appendChild(option);
     });
-    myFace.srcObject = myStream;
   } catch (e) {
     console.log(e);
   }
 }
 
+async function getMedia(videoDeviceId) {
+  const initialConstraints = {
+    audio: true,
+    video: { facingMode: "user" },
+  };
+  const cameraConstraints = {
+    audio: true,
+    video: { deviceId: { exact: videoDeviceId } },
+  };
+  try {
+    myStream = await navigator.mediaDevices.getUserMedia(
+      videoDeviceId ? cameraConstraints : initialConstraints
+    );
+    myFace.srcObject = myStream;
+    if (!videoDeviceId) {
+      await getCameras();
+    }
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+function handleMuteClick() {
+  myStream
+    .getAudioTracks()
+    .forEach((track) => (track.enabled = !track.enabled));
+  // forEach 해주는 이유는 myStream.getAudioTracks()가 array이기 때문
+  // myStream.getAudioTracks()[0].enabled 바꿔주면 해결
+  if (muted) {
+    muteBtn.innerText = "Mute";
+    muted = false;
+  } else {
+    muteBtn.innerText = "Unmute";
+    muted = true;
+  }
+}
+function handleCameraClick() {
+  myStream
+    .getVideoTracks()
+    .forEach((track) => (track.enabled = !track.enabled));
+  if (cameraOff) {
+    cameraBtn.innerText = "Turn Camera OFF";
+    cameraOff = false;
+  } else {
+    cameraBtn.innerText = "Turn Camera ON";
+    cameraOff = true;
+  }
+}
+async function handleCameraChange() {
+  await getMedia(camerasSelect.value);
+}
+
 getMedia();
+
+muteBtn.addEventListener("click", handleMuteClick);
+cameraBtn.addEventListener("click", handleCameraClick);
+camerasSelect.addEventListener("input", handleCameraChange);
